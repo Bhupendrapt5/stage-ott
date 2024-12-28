21d4c2a61509af214e99f11aab7afc68ad95cb4b
Bhupendra Patel <m0rph3us@Bhupendras-MacBook-Pro.local>
2024-12-28 11:07:12 +0530

    Add packages and home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stage_ott_assignment/views/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ProviderScope(child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,15 +13,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
         ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen());
   }
 }
